db9bbf1875a34ba654a3b3f64f3204d3a4c632e3
Salma-abdirahman <111789352+Salma-abdirahman@users.noreply.github.com>
2023-02-04 16:50:13 +0000

    adding fixed js

---
+++ static/js/logic.js
@@ -0,0 +1,57 @@
+// Leaflet Part 1: Earthquake visualisation
+
+
+// Creating the base layers.
+
+//Streetmap
+var streetM = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+})
+
+//topographic
+var topoM = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+	attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+});
+
+// satellite map 
+var satM = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+});
+
+
+
+// Create a baseMaps object.
+var baseMaps = {
+  "Street Map": streetM,
+  "Topographic Map": topoM
+  "Satellite Map": satM
+};
+
+
+// Defining map object.
+var myMap = L.map("map", {
+  center: [-0, -0],
+  zoom: 3,
+  layers: [street]
+});
+
+var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [0, 10, 20, 50, 100, 200, 500, 1000],
+        labels = [];
+
+
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br><br>' : '+');
+    }
+
+    return div;
+};
+
+//Ledgend for the map
+legend.addTo(myMap);
